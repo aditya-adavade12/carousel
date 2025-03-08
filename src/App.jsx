9e3a50bef9c7c0fa207927dad7c81a94f787a44e
Aditya Adavade <adityawork118@gmail.com>
2025-03-08 18:14:59 +0530

    Updated file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Autocarousel from "./Components/autoCarousel";
-import Manualcarousel from "./Components/manualCarousel";
+import Autocarousel from "./Components/AutoCarousel";
+import Manualcarousel from "./Components/Manualcarousel";
 import './App.css'
 const App = () => {
   return (
     <div>
-     <Autocarousel />
      <Manualcarousel />
     </div>
   )
